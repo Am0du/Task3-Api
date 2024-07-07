369f2e6b403bff93bb00cca18c39e327372c70f2
Amodu <amodudavid777@gmail.com>
2024-07-07 12:24:24 +0100

    Feat: Added a command line runner

--- src/main/java/com/example/task3/Task3Application.java
+++ src/main/java/com/example/task3/Task3Application.java
@@ -1,7 +1,12 @@
 package com.example.task3;
 
+import com.example.task3.DAO.UserDAO;
+import com.example.task3.entity.Organisations;
+import com.example.task3.entity.Users;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class Task3Application {
@@ -9,5 +14,12 @@ public class Task3Application {
 	public static void main(String[] args) {
 		SpringApplication.run(Task3Application.class, args);
 	}
-
+//
+//	@Bean
+//	public CommandLineRunner commandLineRunnerRunner(UserDAO userDAO){
+//		return runner ->{
+//			Users user = userDAO.findUser("");
+//			System.out.println(user);
+//		};
+//	}
 }
